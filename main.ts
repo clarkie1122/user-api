108553503351691eab1ebdc9676a810202661222
Ben Clarke <clarkie222012@yahoo.co.uk>
2020-01-20 09:06:24 +0000

    initial commit

---
+++ main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const serverConfig = config.get('server');
+  const logger = new Logger('bootstrap');
+  const app = await NestFactory.create(AppModule);
+
+  // enable cors for development only
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  }
+
+  const PORT = process.env.PORT || serverConfig.port;
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
+}
+bootstrap();
